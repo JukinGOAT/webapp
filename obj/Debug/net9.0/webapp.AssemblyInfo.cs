035a15bc008317c606d68a884cb6b8f2c660d540
JukinGOAT <karlo.dukin3@gmail.com>
2025-05-08 14:09:10 +0200

    radi

--- obj/Debug/net9.0/webapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/webapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c846ec159a6b7b23fd2b5eae9e86ce61ca2585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4edf5c5fc6a80856df250e9597aec4babdda80a")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
