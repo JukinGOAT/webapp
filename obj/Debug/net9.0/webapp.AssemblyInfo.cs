7401ef03efc6d957ffe7e13aa3ba91393e438c5c
JukinGOAT <karlo.dukin3@gmail.com>
2025-05-08 20:46:34 +0200

    slike

--- obj/Debug/net9.0/webapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/webapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+095536a3939a29050415025a8790162c0ddaa362")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+194582d88b3327aa51eac3589fb74997048202f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
