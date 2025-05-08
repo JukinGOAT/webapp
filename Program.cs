5ae2560243aa7b81979cb8217ac17459b5505626
JukinGOAT <karlo.dukin3@gmail.com>
2025-05-08 15:47:25 +0200

    s

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@ namespace webapp
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
