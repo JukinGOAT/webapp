c417d5d129f0dabec1cc79eb437198b81c6cea66
JukinGOAT <karlo.dukin3@gmail.com>
2025-05-09 13:14:16 +0200

    css

--- obj/Debug/net9.0/webapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/webapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23632383df27e452008c5ba90525d6e6651f2f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b3ccd83657ef6a16caeab4f86990332375940d")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
